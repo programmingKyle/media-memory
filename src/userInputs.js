69ffe9f3466da4969e97c8d36ec4a8f4a1ee9c89
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-01-24 17:13:23 -0500

    Add Media Type Selector to Navigation Tabs
    - When the user is on a specific navigation tab, clicking the plus button will display a header that is relevant to that particular tab.
    - Media selection navbar will now show which one is selected

--- src/userInputs.js
+++ src/userInputs.js
@@ -7,6 +7,13 @@ const addMediaCloseButton_el = document.getElementById('addMediaCloseButton');
 const mediaTitleInput_el = document.getElementById('mediaTitleInput');
 const addMediaButton_el = document.getElementById('addMediaButton');
 
+const navMovies_el = document.getElementById('navMovies');
+const navTV_el = document.getElementById('navTV');
+const navBooks_el = document.getElementById('navBooks');
+
+let selectedMedia = 'Movie'; //defaults to Movie
+const mediaTypeHeader_el = document.getElementById('mediaTypeHeader');
+
 searchButton_el.addEventListener('click', () => {
     toggleSearch();
 });
@@ -21,6 +28,7 @@ function toggleSearch(){
 
 addContentButton_el.addEventListener('click', () => {
     addMediaOverlay_el.style.display = 'flex';
+    mediaTypeHeader_el.textContent = `Add ${selectedMedia}`;
 });
 
 addMediaCloseButton_el.addEventListener('click', () => {
@@ -63,4 +71,27 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
 });
-  
+
+const navElements = [navMovies_el, navTV_el, navBooks_el];
+
+function handleNavClick(navElement){
+    navElements.forEach(element => {
+        element.classList.remove('active');
+    });
+    navElement.classList.add('active');
+}
+
+navMovies_el.addEventListener('click', () => {
+    selectedMedia = 'Movie';
+    handleNavClick(navMovies_el);
+});
+
+navTV_el.addEventListener('click', () => {
+    selectedMedia = 'TV Show';
+    handleNavClick(navTV_el);
+});
+
+navBooks_el.addEventListener('click', () => {
+    selectedMedia = 'Book';
+    handleNavClick(navBooks_el);
+});
